0efe2fe5e4826c24afaf481cb536b4a6ae84c34a
jpablocasanueva <jpablocasanueva@gmail.com>
2020-11-19 22:28:24 -0300

    primer commit

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,41 +1,54 @@
 const { io } = require('../server');
-const { TicketControl } = require('../classes/ticket-control')
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utils/utils')
 
-const tickeControl = new TicketControl();
+const usuarios = new Usuarios();
+
 
 io.on('connection', (client) => {
 
-    client.on('siguienteTicket', (data, callback) => {
-        let next = tickeControl.siguiente();
-        console.log(next);
-        callback(next);
-    })
+    client.on('entrarChat', (data, callback) => {
 
-    //emitir evento estado actual
-
-    client.emit('estadoActual', {
-        actual: tickeControl.getLasTicket(),
-        ultimos4: tickeControl.getUltimos4()
-    });
-
-    client.on('atenderTicket', (data, callback) => {
-        if (!data.escritorio) {
+        if (!data.nombre || !data.sala) {
             return callback({
-                err: true,
-                mensaje: 'El escritorio es necesario'
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
             });
         }
 
-        let atenderTicket = tickeControl.atenderTicket(data.escritorio);
+        client.join(data.sala);
 
-        callback(atenderTicket);
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
-        client.broadcast.emit('ultimos4', {
-            ultimos4: tickeControl.getUltimos4()
-        });
+        client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPesonasPorSala(data.sala));
 
+        callback(usuarios.getPesonasPorSala(data.sala));
 
     });
 
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+    });
+
+    client.on('disconnect', () => {
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(personaBorrada.sala).emit(crearMensaje('Admin', `${ personaBorrada.nombre } salio`));
+
+        client.broadcast.to(personaBorrada.sala).emit('listaPersonas', usuarios.getPesonasPorSala(personaBorrada.sala));
+
+    });
+
+    client.on('mensajePrivado', data => {
+
+        let persona = usuarios.getPersona(client.id);
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+
+    })
 
 });
